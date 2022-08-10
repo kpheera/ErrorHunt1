9cee035064c22a7fc3bf4b16f9363194ff07b7ba
Heera K P <kpheera1997@gmail.com>
2022-08-10 17:21:33 +0530

    change in connect

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Library', {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.createConnection(process.env.MONGODB_URI || 'mongodb://localhost:27017/Library', {useUnifiedTopology: true, useNewUrlParser: true});
 const Schema = mongoose.Schema;
 
 

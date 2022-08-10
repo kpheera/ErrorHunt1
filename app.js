620babf31e90b1bd47c45fdd0dd435e52be9aca5
Heera K P <kpheera1997@gmail.com>
2022-08-10 14:04:03 +0530

    Error hunt - Library application

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express'); 
+const path = require ('path'); 
+// point1 cors module is not used and not needed and also have to run npm install before running application.
+const cors = require('cors');
+//point2 inorder to use body parser module, need to require it first
+const bodyParser = require('body-parser');
+
+const nav= [
+    {
+        link:"/books",
+        title:"Books"
+    },
+    {
+        link:"/authors",
+        title:"Authors"
+    },
+    {
+        link:"/addbook",
+        title:"Add Book"
+    },
+    {
+        link:"/addauthor",
+        title:"Add Author"
+    }
+]
+
+const loginRouter = require('./src/routes/loginroute');
+const signupRouter = require('./src/routes/signuproute');
+// point3 changed homeroute to homerouter
+const homeRouter = require('./src/routes/homerouter');
+const booksRouter = require('./src/routes/booksroute');
+const authorsRouter = require('./src/routes/authorsroute');
+
+const app = new express; 
+
+
+app.set('views','./src/views'); 
+app.set('view engine','ejs'); 
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname , '/public'))); 
+
+
+app.use('/login',loginRouter); 
+app.use('/signup',signupRouter); 
+app.use('/home',homeRouter); 
+app.use('/books',booksRouter); 
+app.use('/authors',authorsRouter); 
+
+
+
+app.get('/',function(req,res){
+
+    res.render('index',{});
+    
+});
+
+
+
+
+
+app.listen(5000,()=>{
+    // point5 app listening to port 5000 and in log it was 3000. changed port to 5000 in log
+    console.log("Server Ready on 5000");
+});
